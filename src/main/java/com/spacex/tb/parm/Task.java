dd3e92c290603d25a4cd7154da7c8dd22b62fa67
benben <leezhenhua17@163.com>
2020-08-12 15:16:59 +0800

    a

--- src/main/java/com/spacex/tb/parm/Task.java
+++ src/main/java/com/spacex/tb/parm/Task.java
@@ -1,8 +1,15 @@
 package com.spacex.tb.parm;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.List;
 
+@Data
+@Builder
+@Setter
 public class Task {
     private String operatorId;
     private String projectId;
@@ -36,126 +43,5 @@ public class Task {
     private String modifierId;
     private Date updated;
 
-    public String getOperatorId() {
-        return operatorId;
-    }
 
-    public void setOperatorId(String operatorId) {
-        this.operatorId = operatorId;
-    }
-
-    public String getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(String projectId) {
-        this.projectId = projectId;
-    }
-
-    public String getTemplateId() {
-        return templateId;
-    }
-
-    public void setTemplateId(String templateId) {
-        this.templateId = templateId;
-    }
-
-    public String getTasklistId() {
-        return tasklistId;
-    }
-
-    public void setTasklistId(String tasklistId) {
-        this.tasklistId = tasklistId;
-    }
-
-    public String getTaskgroupId() {
-        return taskgroupId;
-    }
-
-    public void setTaskgroupId(String taskgroupId) {
-        this.taskgroupId = taskgroupId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getExecutorId() {
-        return executorId;
-    }
-
-    public void setExecutorId(String executorId) {
-        this.executorId = executorId;
-    }
-
-    public String getStatusId() {
-        return statusId;
-    }
-
-    public void setStatusId(String statusId) {
-        this.statusId = statusId;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(String dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public String getParentTaskId() {
-        return parentTaskId;
-    }
-
-    public void setParentTaskId(String parentTaskId) {
-        this.parentTaskId = parentTaskId;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "operatorId='" + operatorId + '\'' +
-                ", projectId='" + projectId + '\'' +
-                ", templateId='" + templateId + '\'' +
-                ", tasklistId='" + tasklistId + '\'' +
-                ", taskgroupId='" + taskgroupId + '\'' +
-                ", content='" + content + '\'' +
-                ", executorId='" + executorId + '\'' +
-                ", statusId='" + statusId + '\'' +
-                ", startDate='" + startDate + '\'' +
-                ", dueDate='" + dueDate + '\'' +
-                ", note='" + note + '\'' +
-                ", priority='" + priority + '\'' +
-                ", parentTaskId='" + parentTaskId + '\'' +
-                '}';
-    }
 }
